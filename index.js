431095073d6a16fd994de410e94bea87b11e3a1f
Tim Cecatka <tim.cecatka@gmail.com>
2021-02-10 16:37:26 +0100

    server-client handshake complete: client 'fetches' a post request to the server route '/api' as stingified JSON data, server sendds the respnse back as json as data to the client to be logged onto console

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const express = require ('express');
 const app = express();
 app.listen(3000, () => console.log('Listening to Port 3000'));
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+app.post('/api', (request, response) => {
+  console.log(request.body);
+  // response.end();
+  const data = request.body;
+  response.json({
+    status: 'success',
+    latitude: data.lat,
+    longitude: data.lon
+  });
+});
