fbfdc06f43e771129dba63ed3bcbe602ad72bddb
Tim Cecatka <tim.cecatka@gmail.com>
2021-02-14 16:48:02 +0100

    installed npm nedb for new database and creating an 'Datastore' object in index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require ('express');
+const Datastore = require('nedb');
+
+
 const app = express();
 app.listen(3000, () => console.log('Listening to Port 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+database.insert({ name: 'TimCc', status: 'learning to store on nedb' });
+database.insert({ name: 'Fk', status: 'Fotos' });
 
 app.post('/api', (request, response) => {
   console.log(request.body);
