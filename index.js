f008a3b30cdeaad8a492bd583f837f886582f072
Tim Cecatka <tim.cecatka@gmail.com>
2021-02-15 21:44:25 +0100

    added submit button with an onclick async function to trigger the .fetch method to send the Geolocation data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const fs = require('fs');
 const Datastore = require('nedb');
 
 
@@ -6,6 +7,7 @@ const app = express();
 app.listen(3000, () => console.log('Listening to Port 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
+
 
 const database = new Datastore('database.db');
 database.loadDatabase();
@@ -16,6 +18,16 @@ app.post('/api', (request, response) => {
   console.log(request.body);
   // response.end();
   const data = request.body;
+  // console.log("\nFile Contents of file before append:", 
+  //   fs.readFileSync("submittedGeolocation.txt", "utf8")); 
+  // fs.appendFile('submittedGeolocation.txt', `${data}`, 'utf8',
+  //   (err) => {
+  //     if (err) { 
+  //       console.log(err);
+  //     } else {
+  //     console.log("The file \"submittedGeolocation.json\" was created or updated",
+  //       fs.readFileSync("submittedGeolocation.txt", "utf8"));
+  //   });
   response.json({
     status: 'success',
     latitude: data.lat,
