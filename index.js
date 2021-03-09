3c360d35c6eb85c8baec4c5e41ca826c202de85d
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-09 20:57:08 +0100

    show database on client /api

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require ('express');
-// const fs = require('fs');
 const Datastore = require('nedb');
 
 
 const app = express();
 app.listen(3000, () => console.log('Listening to Port 3000'));
-// app.use(express.urlencoded({
-//   extended: true;
-// }))
 app.use(express.static('public'));
 app.use(express.json({limit: '100kb'}));
 
 
 const database = new Datastore('database.db');
 database.loadDatabase();
+
+app.get('/api', (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  })
+});
 
 app.post('/api', (request, response) => {
 
